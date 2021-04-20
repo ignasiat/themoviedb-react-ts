cdd2f3663d0cd0e243cec9f4bad5008cc143b60f
ignasi <amargos.ignasi@gmail.com>
2021-04-20 10:49:57 +0200

    Fix action type in loadPopularMovies

--- src/redux/actions/mediaActionCreators.ts
+++ src/redux/actions/mediaActionCreators.ts
@@ -1,12 +1,13 @@
 import axios from 'axios'
 import mediaActionTypes from './mediaActionTypes'
 import apiConstants from '../../constants/apiConstants'
+import {Dispatch} from 'redux'
 
 export function loadPopularMovies () {
-  return async (dispatch) => {
-    const { results } = await axios.get(`${apiConstants.URL_POPULAR_MOVIES}${apiConstants.QUERY_API_KEY}${process.env.REACT_APP_APIKEY}`)
+  return async (dispatch: Dispatch) => {
+    const { data: {results} } = await axios.get(`${apiConstants.URL_POPULAR_MOVIES}${apiConstants.QUERY_API_KEY}${process.env.REACT_APP_APIKEY}`)
     dispatch({
-      type: mediaActionTypes,
+      type: mediaActionTypes.LOAD_POPULAR_MOVIES,
       data: results
     })
   }
