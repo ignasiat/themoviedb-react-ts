dc93ae483896eff2209d0cb69157db12ef8e4104
ignasi <amargos.ignasi@gmail.com>
2021-04-20 11:02:27 +0200

    Add App page with real data

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+import { loadPopularMovies } from '../../redux/actions/mediaActionCreators'
+import { RootState } from '../../redux/reducers';
+
+const mapStateToProps = (state: RootState ) => ({
+    media: state.media
+});
+
+const mapDispatchToProps = (dispatch:Dispatch) => ({
+  actions:
+    bindActionCreators({
+      loadPopularMovies
+    }, dispatch)
+})
+  
+type Props = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
+
+
+const UnconnectedApp: React.FC<Props> = ({ media, actions }: any) => {
+
+  useEffect(()=> {
+    actions.loadPopularMovies();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      {media.length && media.map((element: any) => (<p key={element.id}>{element.id}</p>) )}
+    </div>
+  );
+}
+
+
+export const App = connect(mapStateToProps, mapDispatchToProps)(UnconnectedApp);
