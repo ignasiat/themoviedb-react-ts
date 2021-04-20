0fe7a4cbb44197b1fc237e4d0d7d72d92cf2022d
ignasi <amargos.ignasi@gmail.com>
2021-04-20 11:31:37 +0200

    Install and configure eslint. Formating existing files

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators, Dispatch } from 'redux';
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators, Dispatch } from 'redux'
 import { loadPopularMovies } from '../../redux/actions/mediaActionCreators'
-import { RootState } from '../../redux/reducers';
+import { RootState } from '../../redux/reducers'
 
-const mapStateToProps = (state: RootState ) => ({
-    media: state.media
-});
+const mapStateToProps = (state: RootState) => ({
+  media: state.media
+})
 
 const mapDispatchToProps = (dispatch:Dispatch) => ({
   actions:
@@ -14,26 +14,21 @@ const mapDispatchToProps = (dispatch:Dispatch) => ({
       loadPopularMovies
     }, dispatch)
 })
-  
+
 type Props = ReturnType<typeof mapStateToProps> &
   ReturnType<typeof mapDispatchToProps>;
 
-
 const UnconnectedApp: React.FC<Props> = ({ media, actions }: any) => {
-
-  useEffect(()=> {
-    actions.loadPopularMovies();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    actions.loadPopularMovies()
   }, [])
-
 
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      {media.length && media.map((element: any) => (<p key={element.id}>{element.id}</p>) )}
+      {media.length && media.map((element: any) => (<p key={element.id}>{element.id}</p>))}
     </div>
-  );
+  )
 }
 
-
-export const App = connect(mapStateToProps, mapDispatchToProps)(UnconnectedApp);
+export const App = connect(mapStateToProps, mapDispatchToProps)(UnconnectedApp)
