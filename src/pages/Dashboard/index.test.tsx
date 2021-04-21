b5591eaf0ade1034dca02d246f2e88bae4ce322c
ignasi <amargos.ignasi@gmail.com>
2021-04-21 22:49:44 +0200

    Add tests for Dashboard component

--- src/pages/Dashboard/index.test.tsx
+++ src/pages/Dashboard/index.test.tsx
@@ -1,9 +1,67 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { act, render, fireEvent } from '@testing-library/react'
 import { Dashboard } from '.'
+import store from '../../redux/store/configureStore'
+import { Provider } from 'react-redux'
+import constants from '../../constants'
 
-test('renders learn react link', () => {
-  render(<Dashboard />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+describe('Given a Dashboard component', () => {
+  let container : HTMLDivElement | null | any
+
+  beforeEach(() => {
+    container = document.createElement('div')
+    document.body.appendChild(container)
+  })
+
+  afterEach(() => {
+    container = null
+  })
+
+  describe('When is invoked', () => {
+    test('Then should render a h1', () => {
+      act(() => {
+        render(
+          <Provider store={store}>
+            <Dashboard />
+          </Provider>, container)
+      })
+
+      const title: HTMLHeadingElement | null | any = document.querySelector('h1')
+      expect(title.innerHTML).toBe(constants.movie.toUpperCase())
+    })
+  })
+
+  describe('When tv button is clicked', () => {
+    test('Then should render a h1 with content TV', () => {
+      act(() => {
+        render(
+          <Provider store={store}>
+            <Dashboard />
+          </Provider>, container)
+      })
+
+      const button = document.querySelectorAll('button')
+      fireEvent.click(button[1])
+
+      const title: HTMLHeadingElement | null | any = document.querySelector('h1')
+      expect(title.innerHTML).toBe(constants.tv.toUpperCase())
+    })
+  })
+
+  describe('When movie button is clicked', () => {
+    test('Then should render a h1 with content MOVIE', () => {
+      act(() => {
+        render(
+          <Provider store={store}>
+            <Dashboard />
+          </Provider>, container)
+      })
+
+      const button = document.querySelectorAll('button')
+      fireEvent.click(button[0])
+
+      const title: HTMLHeadingElement | null | any = document.querySelector('h1')
+      expect(title.innerHTML).toBe(constants.movie.toUpperCase())
+    })
+  })
 })
