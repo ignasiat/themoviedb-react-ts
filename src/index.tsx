88b1385206cbe89ee8912081bae141c5fdd39956
ignasi <amargos.ignasi@gmail.com>
2021-04-21 10:28:36 +0200

    Added reset styles file to index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import './index.scss'
 import { Provider } from 'react-redux'
 import configureStore from './redux/store/configureStore'
 import {
@@ -8,11 +8,13 @@ import {
 } from 'react-router-dom'
 import { Dashboard } from './pages/Dashboard'
 import { Detail } from './pages/Detail'
+import Header from './component/Header'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={configureStore}>
     <BrowserRouter>
+      <Header />
         <Switch>
           <Route path="/" exact component={Dashboard} />
           <Route path="/detail/:mediaType/:id" component={Detail} />
